470fd164dfe0facb8fe9c191dfe7573aad15e1a4
Francis Beeson <leobeeson@gmail.com>
2022-08-21 00:20:48 +0100

    Add base case test for when letters are missing

--- DynamicProgramming/Abbreviation.py
+++ DynamicProgramming/Abbreviation.py
@@ -1,8 +1,18 @@
+import pytest
 
 
 def abbreviation(a: str, b:str) -> str:
-    pass
+    candidates = set(a)
+    for letter in b:
+        if letter not in candidates:
+            return "NO"
 
+
+def test_b_has_letters_missing_in_a() -> None:
+    a = "AbcDE"
+    b = "AFDE"
+    result = abbreviation(a, b)
+    assert result == "NO"
 
 
 if __name__ == '__main__':
@@ -13,6 +23,7 @@ if __name__ == '__main__':
         b = input()
         result = abbreviation(a, b)
         print(result + "\n")
+
 
 
 
