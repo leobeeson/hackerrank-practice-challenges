aa62e4c64bda45cd298f3fd48d22972811190b4d
Francis Beeson <leobeeson@gmail.com>
2022-08-20 23:56:24 +0100

    Set up ingestion of parameters and dummy abbreviation function

---
+++ DynamicProgramming/Abbreviation.py
@@ -0,0 +1,15 @@
+
+
+def abbreviation(a: str, b:str) -> str:
+    pass
+
+
+
+if __name__ == '__main__':
+    num_queries = int(input().strip())
+
+    for query in range(num_queries):
+        a = input()
+        b = input()
+        result = abbreviation(a, b)
+        print(result + "\n")
